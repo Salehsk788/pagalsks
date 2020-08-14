1f45924b13dd758938f3fa8515fe948411f0aa9f
Salehsk788 <69680329+Salehsk788@users.noreply.github.com>
2020-08-14 18:22:23 +0500

    Add files via upload

--- zeroboy.py
+++ zeroboy.py
@@ -412,7 +412,7 @@ def pilih_super():
 	print '\033[1;96m[\033[1;97m✓\033[1;96m] \033[1;92mProcess Has Been Completed \033[1;97m....'
 	print"\033[1;96m[+] \033[1;92mTotal OK/\x1b[1;93mCP \033[1;91m: \033[1;92m"+str(len(oks))+"\033[1;97m/\033[1;93m"+str(len(cekpoint))
 	print("\033[1;96m[+] \033[1;92mCP File Has Been Saved \033[1;91m: \033[1;97mout/checkpoint.txt")
-	raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
+	raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")👐
 	menu()
 
 if __name__ == '__Sk__':
